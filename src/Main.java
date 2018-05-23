300d3b4041f82297c634beee8273bc5064e50ba4
Cleverson Nahum <cleversonahum@gmail.com>
2018-05-23 15:17:51 -0300

    trying to instantiate Blockchain

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import server.HTTP;
-import blockchain.Blockchain;
-import blockchain.Block;
+import blockchain.*;
+import transaction.*;
 
-//ERASE AFTER
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -12,22 +8,7 @@ public class Main {
 	    System.out.println("Hello World!");
 	    
 	    Blockchain blockchain = new Blockchain();
-	    Block teste = blockchain.getLastBlock();
-	    //System.out.println("Index: "+teste.index+"\nHASH: "+teste.hash);
-	    Block teste2 = blockchain.generateNextBlock("teste");
-	    //System.out.println("Index: "+teste2.index+"\nHASH: "+teste2.hash+"\nPHASH: "+teste2.previousHash);
-	    try{
-        TimeUnit.SECONDS.sleep(30);
-} catch(Exception e){}
-	    Block teste3 = blockchain.generateNextBlock("teste2");
-	    blockchain.printBlockchain(blockchain.getBlockchain());
-
-
-
-//	    HTTP server = new HTTP();
-//	    try{
-//	    server.start();
-//        }catch(Exception e){e.printStackTrace();}
+	    TxPool txPool = new TxPool();
 
     }
     

0e4a3b38a20e7c1901b0c3c3e8b29cb4603a7fe0
bruno-ribeiro <up201505817@fe.up.pt>
2018-05-27 13:27:35 +0100

    Update TxIn.java

--- src/transaction/TxIn.java
+++ src/transaction/TxIn.java
@@ -1,6 +1,12 @@
 package transaction;
 
-public class TxIn {
+import java.io.Serializable;
+
+public class TxIn implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -5762734478555076766L;
     public String txOutId;
     public Integer txOutIndex;
     public String signature;
